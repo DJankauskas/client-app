54564b0b9ec01861bf97bb14355f77cdd425dd46
Darius Jankauskas <d5926j@gmail.com>
2018-05-06 18:56:52 -0400

    more changes

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '2.0.400' };
+let VersionInfo = { VERSION : '2.0.931' };
 export default VersionInfo;
